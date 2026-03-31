7020b8863fa95ce79ec64a0f2872debbf8113516
Tomás <usuario@MacBook-Pro-de-Tomas.local>
2026-03-31 15:25:32 +0200

    add Meta Pixel 1262702445994573

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,27 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           href="https://fonts.googleapis.com/css2?family=Unbounded:wght@400;700;900&family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,600;1,300&display=swap"
           rel="stylesheet"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window,document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1262702445994573');
+              fbq('track', 'PageView');
+            `
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style={{display:'none'}}
+            src="https://www.facebook.com/tr?id=1262702445994573&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body>
         <style>{`body { background-color: #FF5C1A; }`}</style>
